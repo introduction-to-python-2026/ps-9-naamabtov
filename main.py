c084e09da53d3cc9c70cae01c6627d02b8536b27
naamabtov <naamabtov@gmail.com>
2025-12-30 15:48:03 +0200

    Add data download commands in main.py
    Added data download commands for parkinsons.csv and lab_setup_do_not_edit.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 cols = ['MDVP:Fo(Hz)', 'MDVP:Fhi(Hz)', 'MDVP:Flo(Hz)', 'status','HNR','MDVP:Shimmer']
