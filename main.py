22c540fca78ef80075571117c700cf584add75b3
naamabtov <naamabtov@gmail.com>
2025-12-30 15:59:20 +0200

    Import pandas and load parkinsons.csv
    Added pandas import and read CSV functionality.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 !wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
 import lab_setup_do_not_edit
 
-
+import pandas as pd
+df = pd.read_csv('/content/parkinsons.csv')
+df.head()
 
 x=df[['MDVP:Fo(Hz)','HNR']]
 y=df['status']
